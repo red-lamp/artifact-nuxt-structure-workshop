79dbe83454f9d49d868696ad6f6003e7ec9296b3
Nattarat Srila <nattarat.design@gmail.com>
2021-02-17 15:26:07 +0700

    Clean file for workshop

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,6 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - artifacts-nuxt-structure',
     title: 'artifacts-nuxt-structure',
     meta: [
       { charset: 'utf-8' },
